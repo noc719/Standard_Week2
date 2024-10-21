906db8d721b173ec7866ab62f0551169cfe2baa3
gwanyoungkim719 <0000kgy@naver.com>
2024-10-21 13:14:51 +0900

    [Add] Quest Manager

---
+++ Standard_Week2_6/Assets/Scripts/QuestManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    private static QuestManager instance;
+
+    public static QuestManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<QuestManager>();
+            }
+            else if (instance == null)
+            {
+                instance = instance.AddComponent<QuestManager>();
+            }
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
